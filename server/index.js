094f7794b9b342c09256c02312c97dbb3dd8a87c
jhdlana <ljh06031@gmail.com>
2024-05-09 21:52:27 +0900

    index.js 생성

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const cookieParser = require('cookie-parser')
+const cookieSession = require('cookie-session')
+const express = require('express')
+const cors = require('cors')
+const connectWithDB = require('./config/db')
+
+require('dotenv').config()
+
+const app = express()
+
+connectWithDB()
+
+app.use(cookieParser())
+
+app.use(cookieSession({
+    name: 'session',
+    maxAge: process.env.COOKIE_TIME * 24 * 60 * 60 * 1000,
+    keys: [process.env.SESSION_SECRET],
+    sameSite: 'none'
+}))
+
+app.use(cors({
+    origin:process.env.CLIENT_URL,
+    credentials: true
+}))
+
+app.use('/', require('./routes'))
+
+app.listen(process.env.PORT || 8000, (err) => {
+    if(err) {
+        console.log('Error in connecting to server : ' + err)
+    }
+
+    console.log(`Server is running on port ${process.env.PORT}}`)
+})
